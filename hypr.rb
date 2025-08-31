623872da2fd61096106c580931fdb614466ee771
Elliott Hillary <git@elliott.thehillarys.uk>
2025-08-31 23:53:18 +0100

    Update

--- hypr.rb
+++ hypr.rb
@@ -1,8 +1,8 @@
 class Hypr < Formula
   desc "Tiny Hyper key daemon"
   homepage "https://github.com/ElliottH/hypr"
-  url "https://github.com/ElliottH/hypr/archive/refs/tags/0.0.1.zip"
-  sha256 "229eeda3799a8ddb9bdf11d88ccaf05fc7d32b6173b660576c96d7a5ab3368c8"
+  url "https://github.com/ElliottH/hypr/archive/refs/tags/0.0.2.zip"
+  sha256 "901af6b03f9477f037715e22bab7cb59b7038479b0cb1fab774682a214703fe8"
   head "https://github.com/ElliottH/hypr.git"
 
   def install
