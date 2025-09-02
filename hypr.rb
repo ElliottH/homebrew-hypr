cebdecc864dc382207cd793bb31523037807263e
Elliott Hillary <git@elliott.thehillarys.uk>
2025-09-02 22:23:44 +0100

    Update to 0.0.3

--- hypr.rb
+++ hypr.rb
@@ -1,14 +1,16 @@
 class Hypr < Formula
   desc "Tiny Hyper key daemon"
   homepage "https://github.com/ElliottH/hypr"
-  url "https://github.com/ElliottH/hypr/archive/refs/tags/0.0.2.zip"
-  sha256 "901af6b03f9477f037715e22bab7cb59b7038479b0cb1fab774682a214703fe8"
-  head "https://github.com/ElliottH/hypr.git"
+  url "https://github.com/ElliottH/hypr/archive/refs/tags/0.0.3.zip"
+  sha256 "c99cd0c39e836dd3ce82cd54f85a37a5e291dde9e15e31c95af8d581ed2c9504"
+  license "MIT"
+
+  depends_on xcode: ["15.2", :build]
+  depends_on :macos
+  depends_on macos: :sonoma
 
   def install
-    ENV.deparallelize
-    system "swift", "build", "-c", "release", "--disable-sandbox"
-    system "codesign", "-fs", "-", "#{buildpath}/.build/release/hypr"
+    system "swift", "build", "--disable-sandbox", "-c", "release", "--static-swift-stdlib"
     bin.install "#{buildpath}/.build/release/hypr"
   end
 
