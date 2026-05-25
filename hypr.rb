545aa12e36ab612d12fa8f384cb402050b36cfd7
Elliott Hillary <git@elliott.thehillarys.uk>
2026-05-25 10:53:26 +0100

    Fix ordering

--- hypr.rb
+++ hypr.rb
@@ -1,10 +1,10 @@
 class Hypr < Formula
   desc "Tiny Hyper key daemon"
   homepage "https://github.com/ElliottH/hypr"
-  url "https://github.com/ElliottH/hypr/releases/download/#{version}/hypr-#{version}-macos.zip"
-  license "MIT"
   version "0.0.4"
   sha256 "9b110ff5a4c23bc5e0ac78acd787fc6ed55ec79cd68d44af22164dcdf1d663f3"
+  url "https://github.com/ElliottH/hypr/releases/download/#{version}/hypr-#{version}-macos.zip"
+  license "MIT"
 
   depends_on :macos
   depends_on macos: :big_sur
